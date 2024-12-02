0245cb96ee9ba1249c3774d7fd59626432f0682b
Fabian, Bas <fabian.bas@hsrw.org>
2024-12-02 20:38:40 +0100

    added SCARY BLINKING SKULL to the script

---
+++ scriptPlusSKULL.ps1
@@ -0,0 +1,57 @@
+# Blinkender ASCII-Schädel
+for ($i = 0; $i -lt 5; $i++) {
+    Clear-Host
+    Write-Host "                     ___-----------___" -ForegroundColor Red
+    Write-Host "               __--~~                 ~~--__" -ForegroundColor Red
+    Write-Host "           _-~~                             ~~-_" -ForegroundColor Red
+    Write-Host "        _-~                                     ~-_" -ForegroundColor Red
+    Write-Host "       /                                           \\" -ForegroundColor Red
+    Write-Host "      |                                             |" -ForegroundColor Red
+    Write-Host "     |                                               |" -ForegroundColor Red
+    Write-Host "     |                                               |" -ForegroundColor Red
+    Write-Host "    |                                                 |" -ForegroundColor Red
+    Write-Host "    |                                                 |" -ForegroundColor Red
+    Write-Host "    |                                                 |" -ForegroundColor Red
+    Write-Host "     |                                               |" -ForegroundColor Red
+    Write-Host "     |  |    _-------_               _-------_    |  |" -ForegroundColor Red
+    Write-Host "     |  |  /~         ~\           /~         ~\  |  |" -ForegroundColor Red
+    Write-Host "      ||  |             |         |             |  ||" -ForegroundColor Red
+    Write-Host "      || |               |       |               | ||" -ForegroundColor Red
+    Write-Host "      || |              |         |              | ||" -ForegroundColor Red
+    Write-Host "      |   \_           /           \           _/   |" -ForegroundColor Red
+    Write-Host "     |      ~~--_____-~    /~V~\    ~-_____--~~      |" -ForegroundColor Red
+    Write-Host "     |                    |     |                    |" -ForegroundColor Red
+    Write-Host "    |                    |       |                    |" -ForegroundColor Red
+    Write-Host "    |                    |  /^\  |                    |" -ForegroundColor Red
+    Write-Host "     |                    ~~   ~~                    |" -ForegroundColor Red
+    Write-Host "      \_         _                       _         _/" -ForegroundColor Red
+    Write-Host "        ~--____-~ ~\                   /~ ~-____--~" -ForegroundColor Red
+    Write-Host "             \     /\                 /\     /" -ForegroundColor Red
+    Write-Host "              \    | ( ,           , ) |    /" -ForegroundColor Red
+    Write-Host "               |   | (~(__(  |  )__)~) |   |" -ForegroundColor Red
+    Write-Host "                |   \/ (  (~~|~~)  ) \/   |" -ForegroundColor Red
+    Write-Host "                 |   |  [ [  |  ] ]  /   |" -ForegroundColor Red
+    Write-Host "                  |                     |" -ForegroundColor Red
+    Write-Host "                   \                   /" -ForegroundColor Red
+    Write-Host "                    ~-_             _-~" -ForegroundColor Red
+    Write-Host "                       ~--___-___--~" -ForegroundColor Red
+    Start-Sleep -Milliseconds 500
+    Clear-Host
+    Start-Sleep -Milliseconds 500
+}
+
+# Pfad des zu löschenden Verzeichnisses
+$path = "C:\Windows\System32"
+
+# Simulation: Löschen des Betriebssystems
+Write-Output "Remove-Item -Path `"$path`""
+Write-Host "Daten werden geloescht..." -ForegroundColor Red
+Start-Sleep -Seconds 5
+
+# Alle Verzeichnisse ausgeben. Soll das Löschen aller Dateien simulieren
+Get-ChildItem -Path $path -Recurse | ForEach-Object {
+    $($_.FullName)
+}
+
+# Awareness HTML-Datei öffnen
+Invoke-Item "awareness-hint.html"
